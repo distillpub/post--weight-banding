655221cec54baff8816e2e2ad14e9822d02e7085
Chelsea Voss <csvoss@openai.com>
2021-04-05 22:15:05 -0700

    Fix pixelatedness of images in Firefox by upscaling them

--- build.py
+++ build.py
@@ -1,17 +1,21 @@
 import base64
-import numpy as np
-from lucid.misc.io.showing import _image_url
+import json
 import os
 from io import BytesIO
+
+import numpy as np
 import PIL
 import PIL.Image
+from lucid.misc.io.showing import _image_url
+from skimage.io import imsave
 
 # NOTE TO SELF: Run this using Python 3.7.
 
+
 def png_url2im(url):
     prefix = "data:image/png;base64,"
-    assert prefix == url[:len(prefix)]
-    img_content = url[len(prefix):]
+    assert prefix == url[: len(prefix)].lower(), url
+    img_content = url[len(prefix) :]
     img_content = BytesIO(base64.b64decode(img_content))
     img = PIL.Image.open(img_content)
     img = np.asarray(img)
@@ -19,15 +23,15 @@ def png_url2im(url):
 
 
 layer_sizes = {
-    "conv2d0" : 64,
-    "conv2d1" : 64,
-    "conv2d2" : 192,
-    "mixed3a" : 256,
-    "mixed3b" : 480,
-    "mixed4a" : 508,
-    "mixed4b" : 512,
-    "mixed4c" : 512,
-    "mixed4d" : 528,
+    "conv2d0": 64,
+    "conv2d1": 64,
+    "conv2d2": 192,
+    "mixed3a": 256,
+    "mixed3b": 480,
+    "mixed4a": 508,
+    "mixed4b": 512,
+    "mixed4c": 512,
+    "mixed4d": 528,
 }
 
 figure_html = {}
@@ -37,8 +41,48 @@ directory = "static/diagrams/"
 build_template = "src/template_index.ejs"
 build_final = "src/index.ejs"
 
+reduced_weights = "static/diagrams/reduced_weights"
+upscaled_weights = "static/diagrams/upscaled_weights"
+
+
+def upscale_image_tag(image_tag: str) -> str:
+    pre, mid, post = image_tag.split('"', 2)
+    arr = png_url2im(mid)
+
+    mult = 8
+
+    if arr.shape[0] <= 5 and arr.shape[1] <= 5:
+        arr = np.repeat(arr, mult, axis=0)
+        arr = np.repeat(arr, mult, axis=1)
+        # new_image_str = _image_url(arr)
+        new_image_str = "data:image/png;base64," + base64.b64encode(arr).decode("utf-8")
+        mid = new_image_str
+
+    image_content = mid[len("data:image/png;base64,") :]
+    image_hash = hex(hash(image_content))[4:20]
+
+    imsave(f"static/generated_images/{image_hash}.png", arr)
+    # with open(f"static/generated_images/{image_hash}.png", "wb") as f:
+    #     f.write(base64.b64decode(image_content))
+    new_mid = f"generated_images/{image_hash}.png"
+
+    return '"'.join([pre, new_mid, post])
+
+
+for f in os.listdir(reduced_weights):
+    if not f.endswith(".json"):
+        continue
+    with open(os.path.join(reduced_weights, f), "r") as infi:
+        with open(os.path.join(upscaled_weights, f), "w") as outfi:
+            contents = json.loads(infi.read())
+            contents["reduced_weights"] = [
+                [upscale_image_tag(image_tag) for image_tag in row]
+                for row in contents["reduced_weights"]
+            ]
+            outfi.write(json.dumps(contents))
+
 for f in os.listdir(directory):
-    if ".svg" not in f:
+    if not f.endswith(".svg"):
         continue
     name = f.split(".")[0]
     key = "diagrams/" + name
@@ -46,23 +90,26 @@ for f in os.listdir(directory):
     print(key, end=": ")
     lines = open(directory + f).read().split("\n")
     if "<svg" in lines[0]:
-        lines[0] = lines[0].replace(">", "id=\"diagram-%s\">" % name)
+        lines[0] = lines[0].replace(">", 'id="diagram-%s">' % name)
         lines[0] = lines[0].replace(">", "style='width: 100%; height: auto;'>")
     for i in range(3):
         lines[i] = lines[i].replace("clip-path", "--disabled-clip-path")
     text = []
     for line in lines:
-        line = line.replace("\"pattern", "\"pattern"+name)
-        line = line.replace("#pattern", "#pattern"+name)
-        line = line.replace("\"image", "\"image"+name)
-        line = line.replace("#image", "#image"+name)
+        line = line.replace('"pattern', '"pattern' + name)
+        line = line.replace("#pattern", "#pattern" + name)
+        line = line.replace('"image', '"image' + name)
+        line = line.replace("#image", "#image" + name)
         if ("<rect" in line or "<path" in line) and "id=" in line:
-            neuron_id = line.split("id=\"")[1].split("\"")[0]
+            neuron_id = line.split('id="')[1].split('"')[0]
             # print("maybe link", neuron_id)
             if "_" in neuron_id and neuron_id.split("_")[0] in layer_sizes:
                 if neuron_id.count("_") > 1:
                     neuron_id = neuron_id[:-2]
-                url = "https://storage.googleapis.com/distill-circuits/inceptionv1-weight-explorer/%s.html" % neuron_id
+                url = (
+                    "https://storage.googleapis.com/distill-circuits/inceptionv1-weight-explorer/%s.html"
+                    % neuron_id
+                )
                 # pattern_n = line.split("#pattern")[1].split(")")[0]
                 # print("link found!", neuron_id)
                 line = line.replace("id=", "class='unit' id=")
@@ -81,13 +128,13 @@ for f in os.listdir(directory):
         else:
             if "<image" in line and "image/png" in line:
                 # and (len(line) > 10000 or (key != "images/rot-features" and len(line) > 10000)):
-                image_str = line.split("xlink:href=\"")[1].split("\"")[0]
-                image_content = image_str[len("data:image/png;base64,"):]
+                image_str = line.split('xlink:href="')[1].split('"')[0]
+                image_content = image_str[len("data:image/png;base64,") :]
                 image_hash = hex(hash(image_content))[4:20]
                 with open(f"static/generated_images/{image_hash}.png", "wb") as f:
                     f.write(base64.b64decode(image_content))
                 line = line.replace(image_str, f"generated_images/{image_hash}.png")
-                print(len(image_content)//1024, end=", ")
+                print(len(image_content) // 1024, end=", ")
             if len(line) < 10000:
                 line = line.replace("/>", " class='pixelated' />")
             # elif "<image" in line:
@@ -108,6 +155,6 @@ for f in os.listdir(directory):
 
 index_template = open(build_template, "r").read()
 for key in figure_html:
-    index_template = index_template.replace(f'{{{key}}}', figure_html[key])
+    index_template = index_template.replace(f"{{{key}}}", figure_html[key])
 
 open(build_final, "w").write(index_template)
